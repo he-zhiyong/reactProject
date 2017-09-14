aab891da69bf75cfe2af1a6ba1066de98894cc83
He-zhiyong <15988134176@163.com>
2017-09-14 13:29:34 +0800

    update package

--- server/index.js
+++ server/index.js
@@ -2,15 +2,15 @@ const express = require('express');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
 const path = require('path');
-//const passport = require('passport');// 用户认证模块passport
+const passport = require('passport');// 用户认证模块passport
 const app = express();
 const port = 3001
 
-//app.use(passport.initialize());// 初始化passport模块
+app.use(passport.initialize());// 初始化passport模块
 app.use(bodyParser.json());// body解析中间件
 app.use(express.static(path.join(__dirname, '../dist')));// 设置静态文件目录
 routes(app);
 
 app.listen(port, function () {
-    console.log(`server listening on port ${port}`);
+    console.log(`server listensing on port ${port}`);
 });
