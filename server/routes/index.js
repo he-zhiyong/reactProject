49c52be7b95fe832c56013ebd007106198878f00
He-zhiyong <15988134176@163.com>
2017-10-10 17:03:18 +0800

    update server

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,6 @@
 function routes(app) {
 	//接口路由
-	const api = ['login', 'register','user'];
+	const api = ['login', 'register','user','proxy'];
 	api.forEach(function (service) {
 		app.use('/api/' + service, require('./api/' + service));
 	});
